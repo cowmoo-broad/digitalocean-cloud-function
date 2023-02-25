cb6f025cdfc1e058f73f3430410fd62c98a00686
moo <105734738+cowmoo-broad@users.noreply.github.com>
2023-02-25 18:25:47 -0500

    Update hello.py

--- packages/sample/hello/hello.py
+++ packages/sample/hello/hello.py
@@ -1,4 +1,9 @@
+from Bio.Seq import Seq
+from Bio.Alphabet import IUPAC
+
 def main(args):
+      my_seq = Seq("MKQHKAMIVALIVICITAVVAALVTRKDLCEVHIRTGQTEVAVF",
+             IUPAC.protein)
       name = args.get("name", "stranger")
       greeting = "Hello test " + name + "!"
       print(greeting)
