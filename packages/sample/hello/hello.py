342cf9adad53490796e54d9eb4d1713b752de721
moo <105734738+cowmoo-broad@users.noreply.github.com>
2023-02-25 23:09:11 -0500

    Update hello.py

--- packages/sample/hello/hello.py
+++ packages/sample/hello/hello.py
@@ -1,11 +1,8 @@
-from Bio.Seq import Seq
-from Bio.Alphabet import IUPAC
+from data.data import OptionQuote, AmazonS3Source
 
 def main(args):
-      my_seq = Seq("MKQHKAMIVALIVICITAVVAALVTRKDLCEVHIRTGQTEVAVF",
-             IUPAC.protein)
-      name = args.get("name", "stranger")
-      greeting = "Hello test " + name + "!"
+      source = AmazonS3Source()
+      greeting = str(source.last_date())
       print(greeting)
       return {"body": greeting}
   
