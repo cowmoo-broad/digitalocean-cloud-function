0c5eba1fe7d8d0acd6472416c09cdf8337c0750f
moo <105734738+cowmoo-broad@users.noreply.github.com>
2023-02-25 23:40:42 -0500

    Update hello.py

--- packages/sample/hello/hello.py
+++ packages/sample/hello/hello.py
@@ -1,8 +1,9 @@
-from data.data import OptionQuote, AmazonS3Source
+#from data.data import OptionQuote, AmazonS3Source
 
 def main(args):
-      source = AmazonS3Source()
-      greeting = str(source.last_date())
+      #source = AmazonS3Source()
+      #greeting = str(source.last_date())
+      greeting = "hello"
       print(greeting)
       return {"body": greeting}
   
